9f7a02417f8de87688b9d4d1cb3d3d565a32bfc7
gizembrasser <gizembrasser101@gmail.com>
2024-01-17 14:25:00 +0100

    add: console.log each shift card

--- scripts/claimShifts.js
+++ scripts/claimShifts.js
@@ -2,15 +2,24 @@ import { By, until } from "selenium-webdriver";
 import login from "./login.js"
 
 const claimShifts = async () => {
+    const driver = await login();
+
     try {
-        const driver = await login();
+        const row = await driver.wait(until.elementLocated(By.css("#main-content > div.container.available-openshifts > div:nth-child(2)")));
 
-        const openShifts = await driver.wait(until.elementLocated(By.css("#main-content > div.container.available-openshifts > div:nth-child(2) > div:nth-child(2) > div > div > div.col-md-4.date.text-center")));
-        const shiftDates = await openShifts.getText();
+        // Find all shift cards within the `row` html element
+        const shiftCards = await row.findElements(By.css(".shift-card.col-3.pr-0"));
 
-        console.log("Text:", shiftDates);
+        // Iterate through each shift card
+        for (const shiftCard of shiftCards) {
+            const shiftInfo = await shiftCard.getText();
+            console.log("Shift info:", shiftInfo)
+        }
+
     } catch (error) {
         console.error("An error occured:", error);
+    } finally {
+        await driver.quit();
     }
 };
 
