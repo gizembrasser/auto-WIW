11cd8d29ed4cd84dcd9578b2d9a8a5a0dcf85080
gizembrasser <gizembrasser101@gmail.com>
2024-01-17 14:36:22 +0100

    better variable names

--- scripts/claimShifts.js
+++ scripts/claimShifts.js
@@ -5,14 +5,15 @@ const claimShifts = async () => {
     const driver = await login();
 
     try {
-        const row = await driver.wait(until.elementLocated(By.css("#main-content > div.container.available-openshifts > div:nth-child(2)")));
+        // Wait for the `row` html element, containing open shifts, to be present
+        const openShifts = await driver.wait(until.elementLocated(By.css("#main-content > div.container.available-openshifts > div:nth-child(2)")));
 
         // Find all shift cards within the `row` html element
-        const shiftCards = await row.findElements(By.css(".shift-card.col-3.pr-0"));
+        const shiftCards = await openShifts.findElements(By.css(".shift-card.col-3.pr-0"));
 
         // Iterate through each shift card
-        for (const shiftCard of shiftCards) {
-            const shiftInfo = await shiftCard.getText();
+        for (const shift of shiftCards) {
+            const shiftInfo = await shift.getText();
             console.log("Shift info:", shiftInfo)
         }
 
