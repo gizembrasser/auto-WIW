9d568a01f85f5a0105285ffb3cd1c30fd701ba99
gizembrasser <gizembrasser101@gmail.com>
2024-01-18 10:37:22 +0100

    update: takes list as argument

--- utils/getNextShiftDates.js
+++ utils/getNextShiftDates.js
@@ -1,31 +1,37 @@
-const getNextShiftDates = (day) => {
+const getNextShiftDates = (weekDays) => {
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
 
     const today = new Date();
-
     // Calculate the date for start of the week that open shifts will be available for
     const openShiftsWeek = new Date(today);
     openShiftsWeek.setDate(today.getDate() + (15 - today.getDay()));
 
-    // Calculate the date for the specified day in the same week as openShiftsWeek
-    const shiftDayIndex = daysOfWeek.indexOf(day);
+    const shifts = [];
 
-    if (shiftDayIndex === -1) {
-        console.log("Please provide a valid day.");
-    } else {
-        const shiftDay = new Date(openShiftsWeek);
-        shiftDay.setDate(openShiftsWeek.getDate() + (shiftDayIndex + 7 - openShiftsWeek.getDay()) % 7);
+    for (const weekDay of weekDays) {
+        // Calculate the date for the specified day in the same week as openShiftsWeek
+        const shiftDayIndex = daysOfWeek.indexOf(weekDay);
 
-        const shift = {
-            month: months[shiftDay.getMonth()],
-            day: shiftDay.getDate().toString().padStart(2, "0")
-        };
+        if (shiftDayIndex === -1) {
+            console.log(`Invalid day: ${weekDay}`);
+        } else {
+            const shiftDay = new Date(openShiftsWeek);
+            shiftDay.setDate(openShiftsWeek.getDate() + (shiftDayIndex + 7 - openShiftsWeek.getDay()) % 7);
 
-        return shift
+            const shift = {
+                month: months[shiftDay.getMonth()],
+                day: shiftDay.getDate().toString().padStart(2, "0")
+            };
+
+            shifts.push(shift);
+        }
     }
+
+    return shifts;
+
 };
 
-console.log(getNextShiftDates("Mo"));
+console.log(getNextShiftDates(["Tuesday", "Friday", "Sunday"]))
 
 export default getNextShiftDates;
