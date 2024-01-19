6f71b26b65a4eff6e47dc6778ee786174df1ae98
gizembrasser <gizembrasser101@gmail.com>
2024-01-19 17:31:01 +0100

    fix: comparison result

--- scripts/claimShifts.js
+++ scripts/claimShifts.js
@@ -15,21 +15,23 @@ const claimShifts = async (targetShifts) => {
         // Iterate through each target shift
         for (const targetShift of targetShifts) {
             // Check if any shift matches the current targetShift
-            const shiftExists = await Promise.all(shiftCards.map(async (shift) => {
+            const openShifts = await Promise.all(shiftCards.map(async (shift) => {
                 // Find the necessary info from the current shift, convert to text
                 const month = await shift.$eval(".col-md-4.date.text-center span", span => span.textContent.trim());
                 const day = await shift.$eval('.col-md-4.date.text-center div', div => div.textContent.trim());
                 const time = await shift.$eval('.row.no-gutters.align-items-center', row => row.textContent.trim());
 
+                const openShift = { month: month, day: day, time: time };
                 // Check if current shifts available matches the targetShift
-                return month === targetShift.month && day === targetShift.day && time === targetShift.time;
+                return Object.entries(targetShift).every(([key, value]) => openShift[key] === value);
             }));
 
             // If a matching shift is found for any targetShift, return true
+            const shiftExists = openShifts.some(Boolean);
             if (shiftExists) {
                 return true;
             }
-        }
+        };
 
         // Otherwise return false
         return false;
