a56a6ebab534935f7969303240358ce037988120
gizembrasser <gizembrasser101@gmail.com>
2024-01-20 18:20:27 +0100

    new logic: refresh page until targetShift has been located

--- scripts/findShifts.js
+++ scripts/findShifts.js
@@ -7,35 +7,51 @@ const findShifts = async (targetShifts) => {
     const { browser, page } = await login();
 
     try {
-        // Wait for the container of open shifts to be present
-        await page.waitForSelector("#main-content > div.container.available-openshifts > div:nth-child(2)");
+        let matchingShifts = [];
+        let timeout = 300000; // 5 minutes timeout
 
-        // Find all shift cards within the container
-        const shiftCards = await page.$$(".shift-card.col-3.pr-0");
+        console.log("Searching for matching open shifts...")
+        while (timeout > 0) {
+            // Wait for the container of open shifts to be present
+            await page.waitForSelector("#main-content > div.container.available-openshifts > div:nth-child(2)");
 
-        const matchingShifts = [];
+            // Find all shift cards within the container
+            const shiftCards = await page.$$(".shift-card.col-3.pr-0");
 
-        // Iterate through each target shift
-        for (const targetShift of targetShifts) {
-            // Check if any shift matches the current targetShift
-            const openShifts = await Promise.all(shiftCards.map(async (shift) => {
-                // Find the necessary info from the current shift, convert to text
-                const month = await shift.$eval(".col-md-4.date.text-center span", span => span.textContent.trim());
-                const day = await shift.$eval('.col-md-4.date.text-center div', div => div.textContent.trim());
-                const time = await shift.$eval('.row.no-gutters.align-items-center', row => row.textContent.trim());
+            // Iterate through each target shift
+            for (const targetShift of targetShifts) {
+                // Check if any shift matches the current targetShift and is not already claimed
+                await Promise.all(shiftCards.map(async (shift) => {
+                    // Find the necessary info from the current shift, convert to text
+                    const month = await shift.$eval(".col-md-4.date.text-center span", span => span.textContent.trim());
+                    const day = await shift.$eval('.col-md-4.date.text-center div', div => div.textContent.trim());
+                    const time = await shift.$eval('.row.no-gutters.align-items-center', row => row.textContent.trim());
 
-                const openShift = { month: month, day: day, time: time };
-                // Check if current shifts available matches the targetShift
-                if (Object.entries(targetShift).every(([key, value]) => openShift[key] === value)) {
-                    return shift;
-                } else {
-                    return null; // Indicate no match
-                }
-            }));
+                    const openShift = { month: month, day: day, time: time };
 
-            // Filter out null values and add the matching open shifts
-            const matchingShift = openShifts.filter(shift => shift !== null);
-            matchingShifts.push(...matchingShift);
+                    // Check if current shift available matches the targetShift
+                    if (Object.entries(targetShift).every(([key, value]) => openShift[key] === value)
+                        &&
+                        !matchingShifts.some(claimed => claimed.month === month && claimed.day === day && claimed.time === time)) {
+
+                        console.log("Shift claimed:", openShift);
+                        matchingShifts.push(openShift);
+                        return openShift;
+                    }
+
+                    return null;
+                }));
+            }
+
+            if (matchingShifts.length === targetShifts.length) {
+                // All targetshifts have matching shifts, break out of the loop
+                break;
+            }
+
+            // No matches found for current targetShift, refresh the page
+            await page.reload({ waitUntil: "domcontentloaded" });
+            timeout -= 2000; // Subtract two seconds from the timeout
+            console.log("Refreshing page...")
         };
 
         return matchingShifts;
