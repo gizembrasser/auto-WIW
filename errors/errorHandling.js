8b0826c4c9f2f84b0388e6a918075292a7562131
gizembrasser <gizembrasser101@gmail.com>
2024-01-20 18:19:33 +0100

    change error message

--- errors/errorHandling.js
+++ errors/errorHandling.js
@@ -1,6 +1,6 @@
 export const noSuchElementErrorHandler = (error) => {
     if (error.name === "NoSuchElementError") {
-        console.error("Email or password input element not found.");
+        console.error("No openshifts found.");
     } else {
         throw error; // Re-throw if it's not the expected error
     }
