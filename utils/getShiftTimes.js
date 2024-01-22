d1dd49ce11ea405ed4481eb9b175ef1dc580b566
gizembrasser <gizembrasser101@gmail.com>
2024-01-22 17:37:17 +0100

    update: shift combinations + weekend

--- utils/getShiftTimes.js
+++ utils/getShiftTimes.js
@@ -1,10 +1,14 @@
-const getShiftTimes = (shiftName) => {
+export const getShiftTimes = (shiftName) => {
     const shiftTimes = {
-        md1: "7:30 - 10:05",
-        md2: "10:15 - 12:50",
+        m1: "7:30 - 10:05",
+        m2: "10:15 - 12:50",
+        m1m2: "7:30 - 12:50",
         s1: "14:15 - 16:50",
         s2: "17:00 - 19:35",
+        s1s2: "14:15 - 19:35",
         s3: "19:45 - 22:20",
+        s2s3: "17:00 - 22:20",
+        s1s2s3: "14:15 - 22:20"
     };
 
     const shiftTime = shiftTimes[shiftName];
@@ -12,8 +16,29 @@ const getShiftTimes = (shiftName) => {
     if (shiftTime) {
         return { time: shiftTime };
     } else {
-        console.log("Provide a valid shift name (md1, md2, s1, s2, s3)");
+        console.log("Provide a valid shift name (m1, m2, s1, s2, s3)");
     }
 };
 
-export default getShiftTimes;
+
+export const getShiftTimesWeekend = (shiftName) => {
+    const shiftTimes = {
+        m1: "8:00 - 10:40",
+        m2: "10:40 - 13:20",
+        m1m2: "8:00 - 13:20",
+        s1: "14:15 - 16:50",
+        s2: "17:00 - 19:35",
+        s1s2: "14:15 - 19:35",
+        s3: "19:45 - 22:20",
+        s2s3: "17:00 - 22:20",
+        s1s2s3: "14:15 - 22:20"
+    };
+
+    const shiftTime = shiftTimes[shiftName];
+
+    if (shiftTime) {
+        return { time: shiftTime };
+    } else {
+        console.log("Provide a valid shift name (m1, m2, s1, s2, s3)");
+    }
+};
