c363fc2a13f426fa7cff9e52e58243ac3228575c
gizembrasser <gizembrasser101@gmail.com>
2024-01-29 14:13:57 +0100

    add: log timer

--- utils/log.js
+++ utils/log.js
@@ -1,16 +1,21 @@
 import fs from "fs";
 
-const logStream = fs.createWriteStream("activity.log", { flags: "a" });
+const logStream = fs.createWriteStream("activity.log", { flags: "w" });
 
 
-const log = (message, isTimeLog = false, customLogStream = null) => {
+const log = (message, isTimeLog = false, timerLabel = null, customLogStream = null) => {
     const stream = customLogStream || logStream
 
     console.log(message); // Log to console
     stream.write(`${message}\n`);
 
     if (isTimeLog) {
-        console.timeLog("Total time elapsed");
+        if (timerLabel) {
+            const elapsedTime = console.timeLog(timerLabel);
+            stream.write(`${timerLabel}: ${elapsedTime}ms\n`);
+        } else {
+            console.time(message); // Start timer
+        }
     }
 };
 
